908d95c005909a717fe135f2712ec69c8f64f574
NKUgpy <g1411856196@163.com>
2021-07-08 21:22:30 +0800

    first

---
+++ app/src/main/java/com/gpy111/app5/firstActivity.java
@@ -0,0 +1,16 @@
+package com.gpy111.app5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gpy111.app5.R;
+
+public class firstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
